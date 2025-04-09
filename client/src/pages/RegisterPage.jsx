d6e768a6f2834b37cf370c96223b72340cc083c3
LaApRental <lanaico@gmail.com>
2025-04-09 16:49:45 +0200

    Update RegisterPage.jsx

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -55,7 +55,7 @@ export function RegisterPage() {
 
             <label className="flex items-start space-x-2 text-sm">
               <input type="checkbox" name="prihvacamUvijete" checked={formData.prihvacamUvijete} onChange={handleChange} className="mt-1" />
-              <span>Pročitao/la sam i slažem se s uvijetima i pravilima oglašavanja na Apartmanija.hr katalozima.</span>
+              <span>Pročitao/la sam i slažem se s uvjetima i pravilima oglašavanja na ____________</span>
             </label>
 
             <button type="submit" className="w-full bg-green-600 text-white p-2 rounded hover:bg-green-700">
