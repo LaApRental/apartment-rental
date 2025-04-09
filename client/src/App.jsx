6464f2666ef4d7fa937e60b8d3890f91d0c317f7
LaApRental <lanaico@gmail.com>
2025-04-09 16:30:16 +0200

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,2 +1,17 @@
-<Route path="/login" element={<LoginPage />} />
-<Route path="/register" element={<RegisterPage />} />
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import { LoginPage } from './pages/LoginPage';
+import { RegisterPage } from './pages/RegisterPage';
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="*" element={<div className="text-center p-10 text-gray-500">404 - Page not found</div>} />
+      </Routes>
+    </Router>
+  );
+}
