94bd0de389306376cae0172406d771dc54d84e26
LaApRental <lanaico@gmail.com>
2025-04-09 17:27:33 +0200

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import { LoginPage } from './pages/LoginPage';
-import { RegisterPage } from './pages/RegisterPage';
+import { RegisterNew } from './pages/RegisterNew';
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
+         <Route path="/register" element={<RegisterNew />} />
         <Route path="*" element={<div className="text-center p-10 text-gray-500">404 - Page not found</div>} />
       </Routes>
     </Router>
