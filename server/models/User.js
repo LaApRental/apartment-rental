209b37e897fc1d9e8fc0a7be11f9a086145d567c
LaApRental <lanaico@gmail.com>
2025-04-10 12:33:29 +0200

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -1,3 +1,16 @@
 const mongoose = require('mongoose');
-const userSchema = new mongoose.Schema({ ... });
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String,
+  ime: String,
+  prezime: String,
+  oib: String,
+  nazivTvrtke: String,
+  adresa: String,
+  postanskiBroj: String,
+  grad: String,
+  mobitel: String,
+  fiksni: String,
+  role: { type: String, default: 'user' }
+});
 module.exports = mongoose.model('User', userSchema);
