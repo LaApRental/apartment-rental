03a6de12117a3089075fb1a56a408400b354b319
LaApRental <lanaico@gmail.com>
2025-04-11 12:26:46 +0200

    Update sendMail.js

--- server/utils/sendMail.js
+++ server/utils/sendMail.js
@@ -1,1 +1,22 @@
+const nodemailer = require('nodemailer');
 
+const transporter = nodemailer.createTransport({
+  service: 'gmail', // or 'zoho'
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  }
+});
+
+async function sendMail(to, subject, text) {
+  const mailOptions = {
+    from: `"Apartmani Hrvatska" <${process.env.EMAIL_USER}>`,
+    to,
+    subject,
+    text
+  };
+
+  await transporter.sendMail(mailOptions);
+}
+
+module.exports = sendMail;
