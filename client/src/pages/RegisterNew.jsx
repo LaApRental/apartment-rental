ad895dfc9b97e4a97ca83656a66586d66d7c5099
LaApRental <lanaico@gmail.com>
2025-04-11 11:45:25 +0200

    Update RegisterNew.jsx

--- client/src/pages/RegisterNew.jsx
+++ client/src/pages/RegisterNew.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-const RegisterPage = () => {
+const RegisterNew = () => {
   const navigate = useNavigate();
   const [userType, setUserType] = useState('privatni');
   const [formData, setFormData] = useState({
@@ -106,4 +106,4 @@ const RegisterPage = () => {
   );
 };
 
-export default RegisterPage;
+export default RegisterNew;
