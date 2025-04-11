5c9df097c844a8d5f42a406ab256159d8e142427
LaApRental <lanaico@gmail.com>
2025-04-11 12:22:48 +0200

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -1,17 +1,21 @@
+// models/User.js
 const mongoose = require('mongoose');
+
 const userSchema = new mongoose.Schema({
-  email: String,
-  password: String,
   ime: String,
   prezime: String,
+  email: { type: String, unique: true },
   oib: String,
   nazivTvrtke: String,
   adresa: String,
   postanskiBroj: String,
   grad: String,
-  mobitel: String,
+  mobilni: String,
   fiksni: String,
   korisnikTip: String,
-  role: { type: String, default: 'user' }
+  activated: { type: Boolean, default: false },
+  activationToken: String,
+  activationExpires: Date
 });
+
 module.exports = mongoose.model('User', userSchema);
