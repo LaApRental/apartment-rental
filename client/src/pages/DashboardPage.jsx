3edd5b50c8bac0ce6377a26105e9716bbdda95c5
LaApRental <lanaico@gmail.com>
2025-04-12 14:59:19 +0200

    Update DashboardPage.jsx

--- client/src/pages/DashboardPage.jsx
+++ client/src/pages/DashboardPage.jsx
@@ -194,7 +194,7 @@ const DashboardPage = () => {
 
         {/* Tab Content */}
 <main className="flex-1 p-4 sm:p-6 md:p-8 overflow-y-auto">
-  <div className="w-full max-w-5xl mx-auto">{renderContent()}</div>
+  <div className="w-full max-w-4xl mx-auto">{renderContent()}</div>
 </main>
         
         
