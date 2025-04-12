216ff2c0ec22edfb3111a1f55f0143ab3959f302
LaApRental <lanaico@gmail.com>
2025-04-12 12:17:58 +0200

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import { LoginPage } from './pages/LoginPage';
 import RegisterNew from './pages/RegisterNew';
 import ActivateAccount from './pages/ActivateAccount';
 import DashboardPage from './pages/DashboardPage'; // ✅ use the new dashboard
+import HostProfile from './pages/HostProfile';
 
 export default function App() {
   return (
@@ -14,6 +15,7 @@ export default function App() {
         <Route path="/register" element={<RegisterNew />} />
         <Route path="/activate" element={<ActivateAccount />} />
         <Route path="/dashboard" element={<DashboardPage />} /> {/* ✅ updated here */}
+        <Route path="/profil" element={<HostProfile />} />
         <Route path="*" element={<div className="text-center p-10 text-gray-500">404 - Page not found</div>} />
       </Routes>
     </Router>
