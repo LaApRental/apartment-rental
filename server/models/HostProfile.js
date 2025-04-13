272fc1aa615233466ca78dc0a99af74470ae5928
LaApRental <lanaico@gmail.com>
2025-04-13 20:57:08 +0200

    Update HostProfile.js

--- server/models/HostProfile.js
+++ server/models/HostProfile.js
@@ -5,7 +5,7 @@ const HostProfileSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true, unique: true },
   firstName: String,
   lastName: String,
-  photo: String, // store image URL or filename
+  photo: String, // can be a URL or file name
   descriptions: { type: Map, of: String },
   translatedStatus: { type: Map, of: String },
 }, { timestamps: true });
